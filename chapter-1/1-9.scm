0ce56744abf48a99b3e52a0ac5b6ea226a85ed7e
Yunlei Liu <ipmsteven@github.com>
2020-10-25 21:09:49 -0500

    add 1.9

---
+++ chapter-1/1-9.scm
@@ -0,0 +1,13 @@
+#lang sicp
+
+;; recursive process
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+;; iterative process
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
